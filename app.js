15187c17aa37bc0a244950cd8d6d4b5d0a1df435
gavinkeo <gavinkeohane@gmail.com>
2026-09-08 15:02:41 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const DATA_URL = "competition.json?v=128";
+const DATA_URL = "competition.json?v=129";
 const PLACEHOLDER_CREST = "crest-placeholder.svg?v=86";
 
 const $ = (sel) => document.querySelector(sel);
